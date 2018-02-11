2e583d2d7ed788061e8ee1c422c908f74fd9c474
Joshua <joshuajamesshier@gmail.com>
2018-02-11 15:55:57 -0800

    fixed some class names so there's no console errors in Category.js

--- client/src/Category.js
+++ client/src/Category.js
@@ -9,8 +9,8 @@ class Category extends Component {
     return (
       <div>
         <h2>1. Choose a category! -- add as many as you want!</h2>
-        <div class="foodChoice">
-          <div class="foodCarousel">
+        <div className="foodChoice">
+          <div className="foodCarousel">
             <Carousel showArrows={true} useKeyboardArrows={true} showThumbs={false} infiniteLoop={true} showIndicators={false} dynamicHeight={true}>
                 <div>
                     <img src={burger} alt="burger"/>
