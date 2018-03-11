695608e8ee2373d80c8479393ee84f48779d61cf
LIONisaQT <ryan.shee@gmail.com>
2018-03-11 13:42:14 -0700

    Fixed bug where incorrect restaurants were showing up

--- client/src/Button.js
+++ client/src/Button.js
@@ -4,16 +4,37 @@ import axios from 'axios';
 class Button extends Component{
     render(){
         return(
-            <button onClick={this.submitAll.bind(this)}>BACK END WUZ HERE</button>
+            <button className="button" onClick={this.submitAll.bind(this)}>Food Now!</button>
         );
     }
 
     submitAll() {
+      var genres = document.getElementById("foodChoices");
+      var termString = sessionStorage.getItem('paramString');
+      var mealType = sessionStorage.getItem('mealType');
+      console.log("term after category, before meal type: " + termString);
+      for(var i = 0; i < genres.children.length; i++){
+        // if(genres.children[i].children[1].checked){
+        //   console.log('checked');
+        // }
+        //console.log(genres.children[i].firstChild.children[0].checked);
+        if(genres.children[i].firstChild.children[0].checked){
+          console.log(genres.children[i].firstChild.firstChild);
+          if(termString === 'undefined'){
+            termString = genres.children[i].firstChild.innerText;
+          }else{
+            termString += "_"+genres.children[i].firstChild.innerText;
+          }
+        }
+      }
+      termString += "_"+mealType;
+      console.log("term after mealtype: " + termString);
+
         // Compile all parameters into JSON object
         var search = {
-          term: localStorage.getItem('term'),
-          radius: localStorage.getItem('radius'),
-          price: localStorage.getItem('price')
+          term: termString,
+          radius: sessionStorage.getItem('radius'),
+          price: sessionStorage.getItem('price')
         };
         console.log(search);
 
@@ -23,13 +44,22 @@ class Button extends Component{
             'Content-Type': 'applications/json',
           },
           params: {
-            term: localStorage.getItem('term'),
-            radius: localStorage.getItem('radius'),
-            price: localStorage.getItem('price')
+            term: termString,
+            radius: sessionStorage.getItem('radius'),
+            price: sessionStorage.getItem('price')
           }
         }).then(function(response) {
             console.log(response.data);
+            console.log(response.data.image_url);
+            document.getElementById("contentHolder").style.display = "none";
+            document.getElementById("outputHolder").style.display = "block";
+            document.getElementById("outputdiv").style.backgroundImage = "url('"+response.data.image_url+"')";
+            document.getElementById("outputName").innerHTML = response.data.name;
+            document.getElementById("outputAddress").innerHTML = ""+response.data.location.address1+", "+response.data.location.city+", "+response.data.location.zip_code+"";
+            //document.getElementById("outputWebsite").innerHTML = response.data.url;
+            document.getElementById("outputPhone").innerHTML = response.data.phone;
             localStorage.setItem('item', JSON.stringify(response.data));
+            sessionStorage.clear();
           }).catch(function(response) {
             console.log('error:');
             console.log(response);
---
+++ client/src/result.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+//import { Carousel } from 'react-responsive-carousel';
+//require ('react-responsive-carousel/lib/styles/carousel.css');
+// import burger from './images/burger.jpg';
+// import nachos from './images/nachos.jpg';
+// import chinese from './images/chinese.jpg';
+import axios from 'axios';
+class Result extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Here's your restaraunt!</h2>
+        <div id="outputdiv">
+          
+        </div>
+
+      </div>
+    );
+  }
+}
+
+  
+
+export default Result;
